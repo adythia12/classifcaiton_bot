901fdf7cd02940da33d1e347f1ea705640cc4ee5
adythia12 <84948089+adythia12@users.noreply.github.com>
2021-06-03 23:34:03 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -33,19 +33,15 @@ bot.onText(/\/predict/, (msg) => {
 bot.on('message', (msg) => {
 	if(state == 1){
 		s = msg.text.split("|");
-		i = parseFloat(s[0])
-		r = parseFloat(s[1])
-		
 		model.predict(
 			[
-				i,
-				r
+			parseFloat(s[0]),
+			parseFloat(s[1])
 			]
 		).then((jres1)=>{
-			v = parseFloat(jres1[0])
-			p = parseFloat(jres1[1])
+			console.log(jres1);
 			
-			cls_model.classify([i, r, v,p]).then((jres2)=>{
+			cls_model.classify([parseFloat(s[0]), parseFloat(s[1]), parseFloat(jres1[0]), parseFloat(jres1[1]).then((jres2)=>{
 				bot.sendMessage(
 					msg.chat.id,
 					`nilai v yang diprediksi adalah ${v} volt`
