9869e9d896c32c845810e9846f37582d6792cfc6
adythia12 <84948089+adythia12@users.noreply.github.com>
2021-06-03 23:11:43 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const cls_model = require('./sdk/cls_model.js');
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '1733547356:AAEOX7oG_z09vS34M-DUHOm5YCPsXYDXohg'
+const token = '1871909542:AAE6OTSe6nt1hzvzZFZGxL5GC-vOzzUlUNU'
 const bot = new TelegramBot(token, {polling: true});
 
 state =0;
